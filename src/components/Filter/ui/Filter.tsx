39dcd3510cc05b8a886545aca784ceb215ed6176
je-delray <hovax74935@ekcsoft.com>
2023-06-11 21:35:45 +0200

    filter edit[0]

--- src/components/Filter/ui/Filter.tsx
+++ src/components/Filter/ui/Filter.tsx
@@ -1,16 +1,25 @@
 import styles from './Filter.module.scss';
-import { list } from '../data/list';
+import { dropdownList, list } from '../data/list';
 
 export function Filter() {
   return (
     <div>
       <div className={styles.filterBtns}>
-        {list.map((item) => (
-          <div className={styles.filterBtn}>
-            {item.text}
-            {<item.icon className={styles.downarrowIcon} />}
-          </div>
-        ))}
+        {list.map((item) => {
+          return (
+            <div>
+              <div className={styles.filterBtn}>
+                {item.text}
+                {<item.icon className={styles.downarrowIcon} />}
+              </div>
+              <div className={styles.dropdown}>
+                {dropdownList.map((item, index) =>
+                  Array(item[0]).map((item) => <p>{item}</p>)
+                )}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
